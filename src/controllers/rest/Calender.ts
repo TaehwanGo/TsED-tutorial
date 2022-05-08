6951f9567504949f26318b2b83e1bdf4a5f657f6
Tony <gth1123@naver.com>
2022-05-08 16:16:37 +0900

    Content-type 헤더 변경

--- src/controllers/rest/Calender.ts
+++ src/controllers/rest/Calender.ts
@@ -1,17 +1,24 @@
 import { BodyParams } from "@tsed/platform-params";
-import { Returns, Put } from "@tsed/schema";
+import { ContentType, Returns } from "@tsed/schema";
 import { Controller } from "@tsed/di";
-
-interface Calendar {
-  id: string;
-  name: string;
-}
 
 @Controller("/calendars")
 export class CalendarCtrl {
-  @Put("/")
-  @Returns(201)
-  create(@BodyParams("name") id: string): Calendar {
-    return { id: "2", name: "test" };
+  @ContentType(".html") // => 'text/html'
+  @ContentType("html") // => 'text/html'
+  @ContentType("json") // => 'application/json'
+  @ContentType("application/json") // => 'application/json'
+  @ContentType("png")
+  getContent(@BodyParams("name") name: string): any {
+    return "something";
+  }
+
+  @Returns(200, String).ContentType(".html") // => 'text/html'
+  @Returns(200, String).ContentType("html") // => 'text/html'
+  @Returns(200, Object).ContentType("json") // => 'application/json'
+  @Returns(200, Object).ContentType("application/json") // => 'application/json'
+  @Returns(200, String).ContentType("png")
+  getContent2(@BodyParams("name") name: string): any {
+    return "something";
   }
 }
